c8bc105d6f785540438c45421dbb9efc2a92bc19
Galath2022 <101233004+Galath2022@users.noreply.github.com>
2024-07-12 15:36:12 +0800

    20240712新编译
    已经安装运行成功。

---
+++ config.json.js
@@ -0,0 +1,12 @@
+{
+"version":"0.59",
+"md5":"f4989cb0454d80f813c29496945d5a6e",
+"home_url":"Module_frpsnew.asp",
+"title":"frps穿透服务器",
+"description":"内网穿透利器，谁用谁知道。",
+"tags":"网络 穿透",
+"author":"Galahad",
+"link":"",
+"changelog":"",
+"build_date":"2024-07-12_13:15:57"
+}
